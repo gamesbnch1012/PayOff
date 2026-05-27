7bbe664bd01e478503492d1333c91f8b577f587a
dhruvtej <gamesbnch1012@gmail.com>
2026-05-27 18:49:00 +0530

    Updated the version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 2
-        versionName = "d-1.06"
+        versionName = "d-1.07"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
